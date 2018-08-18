7b2cc95145cd0496d713f35712c94a2bc36fe869
luansheng <luansheng@gmail.com>
2018-08-18 21:24:06 +0800

    add a new paramere:"direct" to control the direction of pedigree trace.

--- R/keepped.R
+++ R/keepped.R
@@ -1,64 +1,120 @@
 #' @import data.table
-keepped <- function(ped, cand=NULL, gen=NULL, num=FALSE){
-  ped_check <- checkped(ped)
-  #pruning the pedigree by candidate
-  if (!is.null(cand)) {
-
-    if (!(class(cand) %in% "character" )) {
-      cand <- as.character(cand)
-    }
-
-    if (!any(cand %in% ped_check$Ind)) {
-      stop("not find candidate in the pedigree!")
-    }
-    ped_num <- numped(ped_check)
-    i <- 1
-    #backward traverse
-    keep_ind_backward <- match(cand,ped_num$Ind)
-    ind_n <- length(keep_ind_backward) + 1
-    while (length(keep_ind_backward) != ind_n) {
-      ind_n <- length(keep_ind_backward)
-      keep_ind_backward <- unique(c(unlist(ped_num[keep_ind_backward,c("SireNum","DamNum")]),keep_ind_backward))
-      keep_ind_backward <- keep_ind_backward[which(keep_ind_backward > 0)]
-      if (!is.null(gen)){
-        if (i == gen) {
-          break
-        }
+keepped <-
+  function(ped,
+           cand = NULL,
+           direct = "all",
+           gen = NULL,
+           num = FALSE) {
+    ped_check <- checkped(ped)
+    #pruning the pedigree by candidate
+    if (!is.null(cand)) {
+      if (!(class(cand) %in% "character")) {
+        cand <- as.character(cand)
       }
-      i <- i+1
-    }
 
-    i <- 1
-    #forward traverse
-    keep_ind_foreward <- match(cand,ped_num$Ind)
-    ind_n <- length(keep_ind_foreward) + 1
-    while (length(keep_ind_foreward) != ind_n) {
-      ind_n <- length(keep_ind_foreward)
-      keep_ind_foreward <- unique(c(ped_num[which(SireNum %in% keep_ind_foreward),IndNum],
-                                    ped_num[which(DamNum %in% keep_ind_foreward),IndNum],keep_ind_foreward))
-      keep_ind_foreward <- keep_ind_foreward[which(keep_ind_foreward > 0)]
-      if (!is.null(gen)){
-        if (i == gen) {
-          break
-        }
+      if (!any(cand %in% ped_check$Ind)) {
+        stop("not find candidate in the pedigree!")
       }
-      i <- i+1
+      ped_num <- numped(ped_check)
+      if (direct %in% c("all")) {
+        i <- 1
+        #backward traverse
+        keep_ind_backward <- match(cand, ped_num$Ind)
+        ind_n <- length(keep_ind_backward) + 1
+        while (length(keep_ind_backward) != ind_n) {
+          ind_n <- length(keep_ind_backward)
+          keep_ind_backward <-
+            unique(c(unlist(ped_num[keep_ind_backward, c("SireNum", "DamNum")]), keep_ind_backward))
+          keep_ind_backward <-
+            keep_ind_backward[which(keep_ind_backward > 0)]
+          if (!is.null(gen)) {
+            if (i == gen) {
+              break
+            }
+          }
+          i <- i + 1
+        }
+
+        i <- 1
+        #forward traverse
+        keep_ind_foreward <- match(cand, ped_num$Ind)
+        ind_n <- length(keep_ind_foreward) + 1
+        while (length(keep_ind_foreward) != ind_n) {
+          ind_n <- length(keep_ind_foreward)
+          keep_ind_foreward <-
+            unique(c(ped_num[which(SireNum %in% keep_ind_foreward), IndNum],
+                     ped_num[which(DamNum %in% keep_ind_foreward), IndNum], keep_ind_foreward))
+          keep_ind_foreward <-
+            keep_ind_foreward[which(keep_ind_foreward > 0)]
+          if (!is.null(gen)) {
+            if (i == gen) {
+              break
+            }
+          }
+          i <- i + 1
+        }
+        keep_ind <- unique(c(keep_ind_backward, keep_ind_foreward))
+      }
+
+      if (direct %in% c("up")) {
+        #backward traverse
+        i <- 1
+        keep_ind_backward <- match(cand, ped_num$Ind)
+        ind_n <- length(keep_ind_backward) + 1
+        while (length(keep_ind_backward) != ind_n) {
+          ind_n <- length(keep_ind_backward)
+          keep_ind_backward <-
+            unique(c(unlist(ped_num[keep_ind_backward, c("SireNum", "DamNum")]), keep_ind_backward))
+          keep_ind_backward <-
+            keep_ind_backward[which(keep_ind_backward > 0)]
+          if (!is.null(gen)) {
+            if (i == gen) {
+              break
+            }
+          }
+          i <- i + 1
+        }
+        keep_ind <- unique(keep_ind_backward)
+      }
+
+      if (direct %in% c("down")) {
+        #forward traverse
+        i <- 1
+        keep_ind_foreward <- match(cand, ped_num$Ind)
+        ind_n <- length(keep_ind_foreward) + 1
+        while (length(keep_ind_foreward) != ind_n) {
+          ind_n <- length(keep_ind_foreward)
+          keep_ind_foreward <-
+            unique(c(ped_num[which(SireNum %in% keep_ind_foreward), IndNum],
+                     ped_num[which(DamNum %in% keep_ind_foreward), IndNum], keep_ind_foreward))
+          keep_ind_foreward <-
+            keep_ind_foreward[which(keep_ind_foreward > 0)]
+          if (!is.null(gen)) {
+            if (i == gen) {
+              break
+            }
+          }
+          i <- i + 1
+        }
+        keep_ind <- unique(keep_ind_foreward)
+      }
+
+
+      ped_new <- ped_num[sort(keep_ind)]
+      ped_new[, ":="(IndNum = NULL,
+                     SireNum = NULL,
+                     DamNum = NULL)]
+
+      #insure the pruned pedigree with the missing parents.
+      ped_new <- checkped(ped_new)
+
+    } else {
+      ped_new <- ped_check
     }
 
-    keep_ind <- unique(c(keep_ind_backward,keep_ind_foreward))
-    ped_new <- ped_num[sort(keep_ind)]
-    ped_new[,":="(IndNum=NULL,SireNum=NULL,DamNum=NULL)]
-
-    #insure the pruned pedigree with the missing parents.
-    ped_new <- checkped(ped_new)
-
-  } else {
-    ped_new <- ped_check
+    #converting individual, sire, and dam IDs to sequential integer
+    if (num) {
+      ped_new <- numped(ped_new)
+    }
+    return(ped_new)
   }
-
-  #converting individual, sire, and dam IDs to sequential integer
-  if (num) {
-    ped_new <- numped(ped_new)
-  }
-  return(ped_new)
-}
