b0f5c00b16106cf7f522d2d835226e673ab0ed67
luansheng <luansheng@gmail.com>
2018-09-20 10:47:46 +0800

    improve code. use .I to generate sequence; use match() to find sire and dam number.

--- R/numped.R
+++ R/numped.R
@@ -17,16 +17,19 @@ numped <- function(ped) {
   if ("DamNum" %in% ped_new_colnames) {
     ped_new[,DamNum:=NULL]
   }
-  ped_new[,IndNum:=seq(nrow(ped_new))]
+  ped_new[,IndNum:=.I]
   setkey(ped_new,Ind,Sire,Dam)
-  ped_new_num <- merge(ped_new,ped_new[,.(Ind,IndNum)],by.x="Sire",by.y="Ind",all.x=TRUE)
-  setnames(ped_new_num,c("IndNum.x","IndNum.y"),c("IndNum","SireNum"))
-  ped_new_num <- merge(ped_new_num,ped_new_num[,.(Ind,IndNum)],by.x="Dam",by.y="Ind",all.x=TRUE)
-  setnames(ped_new_num,c("IndNum.x","IndNum.y"),c("IndNum","DamNum"))
-  ped_new_num[is.na(SireNum),SireNum:=0]
-  ped_new_num[is.na(DamNum),DamNum:=0]
-  ped_column_name <- colnames(ped_new_num)
+  ped_new_num <- ped_new
+  #ped_new_num <- merge(ped_new,ped_new[,.(Ind,IndNum)],by.x="Sire",by.y="Ind",all.x=TRUE)
+  ped_new[,SireNum:=IndNum[match(Sire,Ind)]]
+  #setnames(ped_new_num,c("IndNum.x","IndNum.y"),c("IndNum","SireNum"))
+  #ped_new_num <- merge(ped_new_num,ped_new_num[,.(Ind,IndNum)],by.x="Dam",by.y="Ind",all.x=TRUE)
+  #setnames(ped_new_num,c("IndNum.x","IndNum.y"),c("IndNum","DamNum"))
+  ped_new[,DamNum:=IndNum[match(Dam,Ind)]]
+  ped_new[is.na(SireNum),SireNum:=0]
+  ped_new[is.na(DamNum),DamNum:=0]
+  ped_column_name <- colnames(ped_new)
   ped_column_name_new <- c(c("Ind","Sire","Dam"),ped_column_name[-which(ped_column_name %chin% c("Ind","Sire","Dam"))])
-  ped_new <- ped_new_num[,ped_column_name_new,with=FALSE]
+  ped_new <- ped_new[, ..ped_column_name_new]
   return(ped_new[order(IndNum)])
 }
