49591b243ae9f00f9b460d83ed182b59627fbf45
luansheng <luansheng@gmail.com>
2018-11-16 13:53:48 +0800

    Rebuilding vignettes due to minor revise for R scripts about detecting devtools and visPedigree packages.

--- inst/doc/tidy-pedigree.R
+++ inst/doc/tidy-pedigree.R
@@ -9,16 +9,12 @@ suppressPackageStartupMessages(is_installed <- require(devtools))
 if (!is_installed) {
   install.packages("devtools")
   suppressPackageStartupMessages(require(devtools))
-} else {
-  suppressPackageStartupMessages(require(devtools))
 }
 
 ## ----installvisPed, eval=TRUE--------------------------------------------
 suppressPackageStartupMessages(is_installed <- require(visPedigree))
 if (!is_installed) {
   install_github("luansheng/visPedigree")  
-  suppressPackageStartupMessages(require(visPedigree))
-} else {
   suppressPackageStartupMessages(require(visPedigree))
 }
 
